3c85446d84d4cdb481d4295c92562f5fb8655f7b
Jorge Fonseca <jorgefonseca503@gmail.com>
2023-11-07 19:22:25 -0600

    Entregable de chakra-UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import Header from "./Header";
+
+function App() {
+  return (
+    
+    <ChakraProvider>
+      <Header />
+      <Box />
+    </ChakraProvider>
+  );
+}
+
+export default App;
